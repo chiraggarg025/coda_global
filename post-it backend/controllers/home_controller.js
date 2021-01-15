6e0816763385b5ad08f068f6202b1772b459d4a8
Chanakya Garg <chiraggarg025@gmail.com>
2021-01-15 12:46:03 +0530

    Update home_controller.js

--- post-it backend/controllers/home_controller.js
+++ post-it backend/controllers/home_controller.js
@@ -1,7 +1,7 @@
 // just a welcome api
 module.exports.home = function(req,res){
     return res.json(200,{
-        message:"Welcome to Doctor's App",
+        message:"Welcome to Coda_Global App",
         
     })
 }
